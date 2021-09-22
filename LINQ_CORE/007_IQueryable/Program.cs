45955c975ec84eba299dec1c8e56fd9246b90b82
e.dudkomykola@gmail.com <e.dudkomykola@gmail.com>
2021-09-22 16:00:23 +0200

    Pridani modeluu

---
+++ LINQ_CORE/007_IQueryable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries_LINQ_to_Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (Context db = new Context())
+            {
+                //IEnumerable - da obycajne selecet dotas a potom filtruje v code  podla where
+                // - tu dostanes vsekty data
+
+                int id = 1;
+                IEnumerable<Phone> phoneIEnum = db.Phones;
+                var phones = phoneIEnum.Where(p => p.Id > id).ToList();
+            }
+
+            using (Context db = new Context())
+            {
+                //IQueryable - da dotaz aj s podminku na stranu databaze
+                //- tu dostanes len vyfitrovane data 
+
+                int id = 1;
+                IQueryable<Phone> phoneIQuer = db.Phones;
+                var phones = phoneIQuer.Where(p => p.Id > id).ToList();
+            }
+
+            using (Context db = new Context())
+            {
+                IQueryable<Phone> phoneIQuer = db.Phones;
+                phoneIQuer = phoneIQuer.Where(p => p.CompanyId == 1);
+                phoneIQuer = phoneIQuer.Where(p => p.Price < 50000);
+                var phones = phoneIQuer.ToList();
+            }
+        }
+    }
+}
---
+++ LINQ_CORE/Aggregate operations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Queries_LINQ_to_Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Any - podmineka ktoa dava true ked aspon jeden nazov zhoduje
+            // All - podmineka ked dava true ked sa vsetky nazvy shoduju 
+            using (Context db = new Context())
+            {
+               bool result1 = db.Phones.Any(p => p.Company.Name == "Samsung"); //Any()
+
+               bool result2 = db.Phones.All(p => p.Company.Name == "Huawei"); //All() 
+            }
+            
+
+            using (Context db = new Context())
+            {
+                int number1 = db.Phones.Count();
+
+                int number2 = db.Phones.Count(p => p.Name.Contains("Samsung"));
+
+                Console.WriteLine(number1);
+                Console.WriteLine(number2);
+            }
+
+            using (Context db = new Context())
+            {
+                // Minimalna cena
+                int minPrice = db.Phones.Min(p => p.Price);
+                // Maximalna cena
+                int maxPrice = db.Phones.Max(p => p.Price);
+                // Stredna cena od firmy Samsung
+                double avgPrice = db.Phones.Where(p => p.Company.Name == "Samsung")
+                    .Average(p => p.Price);
+
+                Console.WriteLine(minPrice);
+                Console.WriteLine(maxPrice);
+                Console.WriteLine(avgPrice);
+            }
+
+            using (Context db = new Context())
+            {
+                // Celkova cena
+                int sum1 = db.Phones.Sum(p => p.Price);
+                // Celkova cena Samsung
+                int sum2 = db.Phones.Where(p => p.Name.Contains("Samsung"))
+                    .Sum(p => p.Price);
+                Console.WriteLine(sum1);
+                Console.WriteLine(sum2);
+            }
+        }
+    }
+}
